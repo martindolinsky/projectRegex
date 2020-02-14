e3e6b867b18eba6ccac642a6bfdc485943282e83
Martin Dolinsky <martin.dolinsky@akademiasovy.sk>
2020-02-14 10:04:18 +0100

    evetything working

--- src/sk/itsovy/regex/Main.java
+++ src/sk/itsovy/regex/Main.java
@@ -1,9 +1,10 @@
 package sk.itsovy.regex;
 
 import sk.itsovy.data.DataIP;
+import sk.itsovy.data.Date;
+import sk.itsovy.data.MacAddress;
 import sk.itsovy.data.PersonalID;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,9 +14,15 @@ public class Main {
     public static void main(String[] args) {
 		DataIP dataIP = new DataIP();
 		PersonalID personalID = new PersonalID();
+		MacAddress macAddress = new MacAddress();
+		Date date = new Date();
 		ipAddressRegex(dataIP.get());
 		System.out.println();
 		personalIDRegex(personalID.get());
+		System.out.println();
+		macAddressRegex(macAddress.get());
+		System.out.println();
+		dateRegex(date.get());
     }
 
     public static void ipAddressRegex(List<String> list) {
@@ -39,6 +46,32 @@ public class Main {
 				Matcher matcher = pattern.matcher(s);
 				if (matcher.find()) {
 					System.out.println("VALID ID: " + matcher.group());
+				}
+			}
+		}
+	}
+
+	public static void macAddressRegex(List<String> list) {
+		Pattern pattern = Pattern.compile(
+				"^([0-9A-Fa-f][0-9A-Fa-f][\\:]){5}[0-9A-Fa-f]{2}$");
+		for (String s: list) {
+			if (s != null) {
+				Matcher matcher = pattern.matcher(s);
+				if (matcher.find()) {
+					System.out.println("VALID MAC: " + matcher.group());
+				}
+			}
+		}
+	}
+
+	public static void dateRegex(List<String> list) {
+		Pattern pattern = Pattern.compile(
+				"^((([1-9]|[12][0-9]|3[0-1])\\.(1|3|5|7|8|10|12))|(([1-9]|[12][0-9]|30)\\.(4|6|9|11))|(([1-9]|[12][0-9])\\.2))\\.(19|20)[0-9]{2}$");
+		for (String s: list) {
+			if (s != null) {
+				Matcher matcher = pattern.matcher(s);
+				if (matcher.find()) {
+					System.out.println("VALID DATE: " + matcher.group());
 				}
 			}
 		}
